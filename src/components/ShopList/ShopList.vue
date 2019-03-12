3d73d6414f554f3aff3bec3c67960640bfd6d63f
Jay <12123131@qq.com>
2019-03-12 10:50:03 +0800

    星星评价抽出为单独一个组件

--- src/components/ShopList/ShopList.vue
+++ src/components/ShopList/ShopList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shop_container">
-    <ul class="shop_list">
+    <ul class="shop_list" v-if="shops.length">
       <li class="shop_li border-1px" v-for="(shop, index) in shops" :key="index">
         <a>
           <div class="shop_left">
@@ -15,13 +15,14 @@
             </section>
             <section class="shop_rating_order">
               <section class="shop_rating_order_left">
-                <div class="star star-24">
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item half"></span>
-                  <span class="star-item off"></span>
-                </div>
+                <!--<div class="star star-24">-->
+                  <!--<span class="star-item on"></span>-->
+                  <!--<span class="star-item on"></span>-->
+                  <!--<span class="star-item on"></span>-->
+                  <!--<span class="star-item half"></span>-->
+                  <!--<span class="star-item off"></span>-->
+                <!--</div>-->
+                <Star :score="shop.rating" :size="24"></Star>
                 <div class="rating_section">
                   {{shop.recent_order_num}}
                 </div>
@@ -44,17 +45,26 @@
         </a>
       </li>
     </ul>
+    <ul v-else>
+      <li v-for="item in 6" :key="item">
+        <img src="./images/shop_back.svg" alt="back">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   import {mapState} from 'vuex'
+  import Star from '../../components/Star/Star.vue'
   export default {
     mounted () {
       this.$store.dispatch('getShops')
     },
     computed: {
       ...mapState(['shops'])
+    },
+    components: {
+      Star
     }
 
   }
---
+++ src/components/Star/Star.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="star" :class="'star-'+size">
+    <span class="star-item" v-for="(starClass, index) in starClasses" :class="starClass" :key="index"></span>
+
+  </div>
+</template>
+
+<script>
+  const CLASS_ON = 'on'
+  const CLASS_HALF = 'half'
+  const CLASS_OFF = 'off'
+
+  export default {
+    props: {
+      score: Number,
+      size: Number
+    },
+    computed: {
+      starClasses () {
+        const {score} = this
+        const scs = []
+        const scoreInteger = Math.floor(score)
+        for (let i = 0; i < scoreInteger; i++) {
+          scs.push(CLASS_ON)
+        }
+        if ((score - scoreInteger) * 10 >= 5) {
+          scs.push(CLASS_HALF)
+        }
+        while (scs.length < 5) {
+          scs.push(CLASS_OFF)
+        }
+
+        return scs
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/Star.styl";
+</style>
