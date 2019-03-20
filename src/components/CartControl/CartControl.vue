90536a86d296c8663fd711c50ed408eb79b5960e
Jay <12123131@qq.com>
2019-03-20 12:01:13 +0800

    添加商品添加/减少按钮及其响应事件

---
+++ src/components/CartControl/CartControl.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="iconfont icon-remove_circle_outline" v-if="food.count" @click="updateFoodCount(false)">
+        <svg class="icon svg-icon" aria-hidden="true" style="width: 1em;height: 1em">
+          <use xlink:href="#icon-buding"></use>
+        </svg>
+      </div>
+    </transition>
+    <div class="cart-count" v-if="food.count">{{food.count}}</div>
+    <div class="iconfont icon-add_circle" @click="updateFoodCount(true)">
+      <svg class="icon svg-icon" aria-hidden="true" style="width: 1em;height: 1em">
+        <use xlink:href="#icon-buding"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      food: Object
+    },
+
+    methods: {
+      updateFoodCount (isAdd) {
+        this.$store.dispatch('updateFoodCount', {isAdd, food: this.food})
+
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../../common/stylus/CartControl.styl";
+</style>
+
+
+
+
+
--- src/pages/Shop/ShopGoods/ShopGoods.vue
+++ src/pages/Shop/ShopGoods/ShopGoods.vue
@@ -4,7 +4,7 @@
       <div class="menu-wrapper">
         <ul>
           <!--current-->
-          <li class="menu-item" v-for="(good, index) in goods" :key="index" :class="{current: index===currentIndex}">
+          <li class="menu-item" v-for="(good, index) in goods" :key="index" :class="{current: index===currentIndex}" @click="clickMenuItem(index)">
             <span class="text bottom-border-1px">
               <img class="icon" :src="good.icon" v-if="good.icon">
               {{good.name}}
@@ -34,7 +34,7 @@
                     <span class="old" v-if="food.oldPrice">￥{{food.oldPrice}}</span>
                   </div>
                   <div class="cartcontrol-wrapper">
-                    <!--<CartControl :food="food"/>-->
+                    <CartControl :food="food"/>
                   </div>
                 </div>
               </li>
@@ -53,6 +53,7 @@
 <script>
   import {mapState} from 'vuex'
   import BScroll from 'better-scroll'
+  import CartControl from '../../../components/CartControl/CartControl.vue'
   export default {
     data () {
       return {
@@ -87,17 +88,19 @@
     methods: {
       // 初始化滚动(加下划线只是为了区别和事件的回调函数)
       _initScrol () {
-        const menuScroll = new BScroll('.menu-wrapper')
-        const foodsScroll = new BScroll('.foods-wrapper',{
+        const menuScroll = new BScroll('.menu-wrapper',{
+          click:true
+        })
+        this.foodsScroll = new BScroll('.foods-wrapper',{
           probeType:2
         })
         // 给右侧列表绑定scroll监听
-        foodsScroll.on('scroll',({x,y}) => { // on的参数为事件名称和事件的回调函数
+        this.foodsScroll.on('scroll',({x,y}) => { // on的参数为事件名称和事件的回调函数
           this.scrollY = Math.abs(y)
         })
         // 因为probeType:2，所以在快速滑动的时候会出现计算出错的情况。所以在不使用3的情况下，需要做些特殊处理
         // 给右侧列表绑定scroll结束的监听
-        foodsScroll.on('scrollEnd',({x,y}) => { // on的参数为事件名称和事件的回调函数
+        this.foodsScroll.on('scrollEnd',({x,y}) => { // on的参数为事件名称和事件的回调函数
           this.scrollY = Math.abs(y)
         })
       },
@@ -112,8 +115,17 @@
           tops.push(top)
         })
         this.tops = tops;
+      },
+      clickMenuItem (index) {
+        //先将左侧的光亮状态更改了(若不写这一句默认是先显示右侧再显示左侧更改)
+        this.scrollY = this.tops[index]
+        //再将右侧的内容的滑动更改了
+        this.foodsScroll.scrollTo(0,-this.tops[index],300)
       }
 
+    },
+    components:{
+      CartControl
     }
   }
 </script>
